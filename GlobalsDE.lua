a692c024002452b73730b3e48f78880d81986895
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-15 20:23:36 +0100

    Create GlobalsDE.lua

---
+++ GlobalsDE.lua
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------------------------
+-- GlobalDE file
+-- Written by Homeopatix
+-- 17 avril 2021
+-- updated 28 decembre 2023
+------------------------------------------------------------------------------------------
+_G.T = {};
+	------------------------------------------------------------------------------------------
+	-- Francais --
+	------------------------------------------------------------------------------------------
+	T[ "PluginName" ] = "ClassObjects";
+	T[ "PluginText" ] = "Von Homeopatix";
+	T[ "Loaded" ] = "Geladen";
+	T[ "Unactivated" ] = "Deaktiviert";
+	T[ "PluginOptionsWindowShow" ] = "Zeigt das Optionsfenster an";
+	------------------------------------------------------------------------------------------
+	-- help --
+	------------------------------------------------------------------------------------------
+	T[ "PluginHelp1" ] = "\n\nListe der Befehle:\n";
+	T[ "PluginHelp2" ] = "/Class show - Schaufenster.\n";
+	T[ "PluginHelp3" ] = "/Class hide - verstecke das Fenster.\n";
+	T[ "PluginHelp4" ] = "/Class options - Zeigen Sie das Optionsfenster an.\n";
+	T[ "PluginHelp5" ] = "/Class esc - Aktivieren oder deaktivieren Sie die Escape-Taste.\n";
+	T[ "PluginHelp6" ] = "/Class clear - L\195\182schen Sie alle Verkn\195\188pfungen.\n";
+	T[ "PluginHelp7" ] = "/Class alt - ALT-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
+	T[ "PluginHelp8" ] = "/Class lock um die Symbole zu sperren oder zu entsperren\n";
+	T[ "PluginHelp9" ] = "/Class toggle - Fenster ein-oder ausblenden\n\n";
+	T[ "PluginHelp10" ] = "Klicken Sie mit der rechten Maustaste auf das Symbol, um das Optionsfeld anzuzeigen";
+	------------------------------------------------------------------------------------------
+	-- command text  --
+	------------------------------------------------------------------------------------------
+	T[ "PluginWindowShow" ] = "Schaufenster.";
+	T[ "PluginWindowHide" ] = "verstecke das Fenster.";
+	T[ "PluginWindowClear" ] = "L\195\182schen Sie alle Verkn\195\188pfungen.";
+	T[ "PluginLocked" ] = "Gesperrte Symbole";
+	T[ "PluginUnlocked" ] = "Symbole freigeschaltet";
+	T[ "PluginEscEnable" ] = "Escape-Taste aktiviert";
+	T[ "PluginEscDesable" ] = "Escape-Taste deaktiviert";
+	T[ "PluginAltEnable" ] = "Alt-Taste aktiviert";
+	T[ "PluginAltDesable" ] = "Alt-Taste deaktiviert";
+	------------------------------------------------------------------------------------------
+	-- Options --
+	------------------------------------------------------------------------------------------
+	T[ "PluginOptionsText" ] = "Optionsfenster";
+	T[ "PluginOption1" ] = "Anzahl der Zeilen";
+	T[ "PluginOption2" ] = "Anzahl der Steckpl\195\164tze pro Zeile";
+	T[ "PluginOption3" ] = " Zeigen Sie den Fensterrand an";
+	T[ "PluginOption4" ] = " Fenster immer eingeschaltet anzeigen";
+	T[ "PluginOptionValidate" ] = "\195\132nderungen validieren";
