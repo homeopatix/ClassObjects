91d5f885fd394e4fcea94623e98a552ce75db266
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-15 20:23:22 +0100

    Create GlobalsEN.lua

---
+++ GlobalsEN.lua
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------------------------
+-- GlobalEN file
+-- Written by Homeopatix
+-- 17 avril 2021
+-- updated 28 decembre 2023
+------------------------------------------------------------------------------------------
+_G.T = {};
+	------------------------------------------------------------------------------------------
+	-- Francais --
+	------------------------------------------------------------------------------------------
+	T[ "PluginName" ] = "ClassObjects";
+	T[ "PluginText" ] = "By Homeopatix";
+	T[ "Loaded" ] = "Loaded";
+	T[ "Unactivated" ] = "Unactivated";
+	T[ "PluginOptionsWindowShow" ] = "Display the options Window";
+	------------------------------------------------------------------------------------------
+	-- help --
+	------------------------------------------------------------------------------------------
+	T[ "PluginHelp1" ] = "\n\nList of all commands:\n";
+	T[ "PluginHelp2" ] = "/Class show - show the window.\n";
+	T[ "PluginHelp3" ] = "/Class hide - hide the window.\n";
+	T[ "PluginHelp4" ] = "/Class options - display the options window.\n";
+	T[ "PluginHelp5" ] = "/Class esc - activate or deactivate the escape key.\n";
+	T[ "PluginHelp6" ] = "/Class alt - Activate or deactivate the alt key to move the icon.\n";
+	T[ "PluginHelp7" ] = "/Class clear - delete all shortcuts.\n";
+	T[ "PluginHelp8" ] = "/Class lock to lock or unlock the icons\n";
+	T[ "PluginHelp9" ] = "/Class toggle - Display or hide the window\n\n";
+	T[ "PluginHelp10" ] = "Right click the icon to show the options panel";
+	------------------------------------------------------------------------------------------
+	-- command text  --
+	------------------------------------------------------------------------------------------
+	T[ "PluginWindowShow" ] = "Show the window.";
+	T[ "PluginWindowHide" ] = "Hide the window.";
+	T[ "PluginWindowClear" ] = "Delete all shortcuts.";
+	T[ "PluginLocked" ] = "Icons Locked";
+	T[ "PluginUnlocked" ] = "Icons Unlocked";
+	T[ "PluginEscEnable" ] = "Escape key Activated";
+	T[ "PluginEscDesable" ] = "Escape key Desactivated";
+	T[ "PluginAltEnable" ] = "Alt key Activated";
+	T[ "PluginAltDesable" ] = "Alt key Desactivated";
+	------------------------------------------------------------------------------------------
+	-- Options --
+	------------------------------------------------------------------------------------------
+	T[ "PluginOptionsText" ] = "Options Window";
+	T[ "PluginOption1" ] = "Number of lines";
+	T[ "PluginOption2" ] = "Number of slots per line";
+	T[ "PluginOption3" ] = " Display the window border";
+	T[ "PluginOption4" ] = " Always display windows";
+	T[ "PluginOptionValidate" ] = "Validate Changes";
