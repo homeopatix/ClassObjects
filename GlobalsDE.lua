b7412a78a760a99bd23ac8ed3f7385509479cef3
Daniel D <spawn900@users.noreply.github.com>
2024-02-18 16:58:23 +0100

    Update GlobalsDE.lua - ClassObjects
    Updated the german translation.

--- GlobalsDE.lua
+++ GlobalsDE.lua
@@ -12,20 +12,20 @@ _G.T = {};
 	T[ "PluginText" ] = "Von Homeopatix";
 	T[ "Loaded" ] = "Geladen";
 	T[ "Unactivated" ] = "Deaktiviert";
-	T[ "PluginOptionsWindowShow" ] = "Zeigt das Optionsfenster an";
+	T[ "PluginOptionsWindowShow" ] = "Optionen werden angezeigt";
 	------------------------------------------------------------------------------------------
 	-- help --
 	------------------------------------------------------------------------------------------
 	T[ "PluginHelp1" ] = "\n\nListe der Befehle:\n";
-	T[ "PluginHelp2" ] = "/Class show - Schaufenster.\n";
-	T[ "PluginHelp3" ] = "/Class hide - verstecke das Fenster.\n";
-	T[ "PluginHelp4" ] = "/Class options - Zeigen Sie das Optionsfenster an.\n";
-	T[ "PluginHelp5" ] = "/Class esc - Aktivieren oder deaktivieren Sie die Escape-Taste.\n";
-	T[ "PluginHelp6" ] = "/Class clear - L\195\182schen Sie alle Verkn\195\188pfungen.\n";
-	T[ "PluginHelp7" ] = "/Class alt - ALT-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
-	T[ "PluginHelp8" ] = "/Class lock um die Symbole zu sperren oder zu entsperren\n";
-	T[ "PluginHelp9" ] = "/Class toggle - Fenster ein-oder ausblenden\n\n";
-	T[ "PluginHelp10" ] = "Klicken Sie mit der rechten Maustaste auf das Symbol, um das Optionsfeld anzuzeigen";
+	T[ "PluginHelp2" ] = "/class show - Zeigt das Fenster an.\n";
+	T[ "PluginHelp3" ] = "/class hide - Blendet das Fenster aus.\n";
+	T[ "PluginHelp4" ] = "/class options - Zeigt die Optionen an.\n";
+	T[ "PluginHelp5" ] = "/class esc - Aktiviert/Deaktiviert die Escape-Taste, um das Fenster zu schlie\195\159en.\n";
+	T[ "PluginHelp6" ] = "/class clear - L\195\182scht alle Verkn\195\188pfungen.\n";
+	T[ "PluginHelp7" ] = "/class alt - Alt-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
+	T[ "PluginHelp8" ] = "/class lock - Sperrt/entsperrt die Verkn\195\188pfungen.\n";
+	T[ "PluginHelp9" ] = "/class toggle - Fenster anzeigen/ausblenden.\n\n";
+	T[ "PluginHelp10" ] = "Klicke mit der rechten Maustaste auf das Symbol, um die Optionen anzuzeigen.";
 	------------------------------------------------------------------------------------------
 	-- command text  --
 	------------------------------------------------------------------------------------------
